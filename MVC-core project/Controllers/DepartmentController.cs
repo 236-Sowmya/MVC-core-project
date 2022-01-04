c862cbdc4f9fb52f289d2fa106c77999250e50e8
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-04 08:57:14 +0530

    Add project files.

---
+++ MVC-core project/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_core_project.Models;
+
+namespace MVC_core_project.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Department> Dept = new List<Department>();
+            Dept.Add(new Department { DeptID = 203, DeptName = "IT", DeptLoc = "Bangalore" });
+            Dept.Add(new Department { DeptID = 204, DeptName = "Marketing", DeptLoc = "Hyderabad" });
+            Dept.Add(new Department { DeptID = 205, DeptName = "Developer", DeptLoc = "Chennai" });
+            ViewData["Dept"] = Dept;
+            return View();
+        }
+    }
+}
---
+++ MVC-core project/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_core_project.Controllers
+{
+    public class StudentController : Controller
+    {
+        public string Index()
+        {
+            return "This is Default action";
+        }
+        public string GetStudentName()
+        {
+            return "Sowmya";
+        }
+    }
+}
